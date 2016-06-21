f5f91e12425bb0720f2bf937d47afb415bf281c0
BruceMacD <brucewmacdonald@gmail.com>
2016-06-20 22:57:46 -0300

    Debug Sprites

--- Assets/Scripts/Dealer.cs
+++ Assets/Scripts/Dealer.cs
@@ -1,24 +1,45 @@
 ﻿using UnityEngine;
-using System;
 using System.Collections.Generic;
 
-public class Dealer : MonoBehaviour {
-
+public class Dealer : MonoBehaviour
+{
+    SpriteRenderer spriteRenderer;
+    bool toggleFace = true;
     //contatiner for cards still in deck
-    public List<GameObject> deck = new List<GameObject>();
+    public List<Sprite> deck = new List<Sprite>();
     //list of cards in play
-    public List<GameObject> cards = new List<GameObject>();
+    public List<Sprite> cards = new List<Sprite>();
     //player's current hand
-    public List<GameObject> player = new List<GameObject>();
+    public List<Sprite> hand = new List<Sprite>();
+    //cardIndex 0 = back
+    public int cardIndex = 1;
 
     public void DealCard()
     {
         Debug.Log("hit");
+        if (toggleFace)
+        {
+            //show card value
+            spriteRenderer.sprite = deck[cardIndex];
+            //this will go out of range
+            cardIndex++;
+        }
+        else
+        {
+            //show back
+            spriteRenderer.sprite = deck[0];
+        }
+
     }
 
     //add cards to deck
     public void Shuffle()
     {
       //cards.Add(c2);
+    }
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 }
---
+++ Assets/Scripts/DebugChangeCard.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugChangeCard : MonoBehaviour
+{
+    Dealer deal;
+    int cardIndex = 1;
+
+    public GameObject card;
+
+	// Use this for initialization
+	void Awake()
+    {
+        deal = card.GetComponent<Dealer>();
+	}
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 100, 28), "Hit me!"))
+        {
+            deal.cardIndex = cardIndex;
+            deal.DealCard();
+
+            if (cardIndex == 52)
+            {
+                cardIndex = 0;
+            }
+
+            cardIndex++;
+        }
+    }
+}
