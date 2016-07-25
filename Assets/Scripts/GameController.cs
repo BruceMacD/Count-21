9ccc8dba3999d406d830faed40fad6eaaa863075
BruceMacD <brucewmacdonald@gmail.com>
2016-07-25 20:17:24 -0300

    Added: betting chips

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -61,7 +61,7 @@ public class GameController : MonoBehaviour
         localScale.x = 0; //hide card
         cardBack.transform.localScale = localScale;
 
-        if (player.HandValue() <= 21)
+        if (player.HandValue() <= 21 && player.HandValue() > dealer.HandValue())
         {
             while (dealer.HandValue() < player.HandValue())
             {
@@ -69,21 +69,22 @@ public class GameController : MonoBehaviour
                 dealer.Push(deck.Pop());
             }
         }
-
+        //let the player see the results
+        yield return new WaitForSeconds(3f);
         CheckWinner();
     }
 
     public void CheckWinner()
     {
-        if (player.HandValue() > dealer.HandValue() && player.HandValue() <= 21)
+        if ((player.HandValue() > dealer.HandValue() && (player.HandValue() <= 21)) || dealer.HandValue() > 21)
         {
-            Debug.Log("Player wins");
+            Debug.Log("Player wins: player = " + player.HandValue() + " dealer: " + dealer.HandValue());
             //TO DO: Add to bank based on bet
         }
         else
         {
             //dealer wins
-            Debug.Log("Dealer wins");
+            Debug.Log("Dealer wins: player = " + player.HandValue() + " dealer: " + dealer.HandValue());
             //Remove bet from play/bank
         }
         ResetGame();
