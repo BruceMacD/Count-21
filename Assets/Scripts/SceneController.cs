e5b1716ba1e8480fe55a8daef427d675b42c7f1a
BruceMacD <brucewmacdonald@gmail.com>
2016-09-02 22:35:00 -0300

    Added: logic for loading scenes from menus

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneController : MonoBehaviour {
+
+    public void SceneLoad(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
